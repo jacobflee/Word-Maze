8773cbd2ce0b3eb14281c89e8c1f22d77b9d9e3a
jacobflee <jacoblee00000@gmail.com>
2020-10-20 03:39:48 -0700

    first commit, hurray!

---
+++ animations/precache-manifest.f7706133b9e6d98a96f5ea6a4c9cf2f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36e7384025962473d77561196de623d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21a8d925e16d3b47ae79",
+    "url": "/static/css/main.2de68ee8.chunk.css"
+  },
+  {
+    "revision": "fa6e84a2ed92f9a4640e",
+    "url": "/static/js/2.b87e8351.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b87e8351.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21a8d925e16d3b47ae79",
+    "url": "/static/js/main.5245e7a7.chunk.js"
+  },
+  {
+    "revision": "55546ce7959de800e26d",
+    "url": "/static/js/runtime-main.0544ca1f.js"
+  }
+]);
